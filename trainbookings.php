5cc12f5879cb341f48aaaf279810f60afb5676f6
klydiiee <57524612+klydiiee@users.noreply.github.com>
2022-03-30 13:23:51 +0800

    Update trainbookings.php

--- trainbookings.php
+++ trainbookings.php
@@ -1,11 +1,24 @@
-<?php
-include 'connect.php' ;
-session_start();
-if ($_SESSION['log'] == '')
-{
-    header("location:sindex.php");
-}
-include 'header.php';
-?>
+<head>  
+           <title>KL TICKET DATABASE</title>  
+            
+      </head>  
+      <body >  
+             <h2><center><b><img src="https://img.icons8.com/nolan/64/database.png"/> USER'S TRAIN BOOKINGS <img src="https://img.icons8.com/ultraviolet/60/000000/train.png"/></center></b></h2>
 
-
+           <div class="container">  
+                <br />  
+                     <table id="database_table" class="table table-striped table-bordered">  
+                          <thead>  
+                            <tr>
+  <th>Date & Time </th>
+  <th>Booking ID </th>
+  <th>Name</th>
+  <th>Source</th>
+  <th>Destination</th>
+  <th>Class</th>
+  <th>Type</th>
+  <th>Amount Paid</th>
+  <th>Download</th>
+   </tr>  
+                          </thead>  
+                          <tbody>
