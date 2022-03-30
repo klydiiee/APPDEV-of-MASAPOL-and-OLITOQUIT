9b4335c077236e051455503c8f2af4c3703e8155
klydiiee <57524612+klydiiee@users.noreply.github.com>
2022-03-30 11:42:37 +0800

    Update register_insert.php

--- register_insert.php
+++ register_insert.php
@@ -1,3 +1,12 @@
+<! Script to add newly registered people in to the Database -->
+
+<style >  .table{  font-family: Montserrat, sans-serif;
+align-items: center}
+
+</style>
+<link rel="stylesheet" type="text/css" href="index.css">
+
+
 <?php
 include 'connect.php';
 include 'head2.php';
