15f35bef51f67a19bcf9f84de19ea7a516fabca2
klydiiee <57524612+klydiiee@users.noreply.github.com>
2022-03-30 13:24:18 +0800

    Update trainbookings.php

--- trainbookings.php
+++ trainbookings.php
@@ -1,3 +1,44 @@
+<?php
+include 'connect.php' ;
+session_start();
+if ($_SESSION['log'] == '')
+{
+    header("location:sindex.php");
+}
+include 'header.php';
+?>
+
+  <div>
+
+  </div>
+<style>
+
+table#database_table {
+    font-size:16px;
+    font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+    border-collapse: collapse;
+    border-spacing: 0;
+}
+
+#database_table td, #database_table th {
+    border: 1px solid #ddd;
+    text-align: left;
+    padding: 8px;
+}
+
+#database_table tr:nth-child(even){background-color: #f2f2f2}
+
+#database_table th {
+    padding-top: 11px;
+    padding-bottom: 11px;
+    background-color: black;
+    color: white;
+}
+
+
+</style>
+
+
 <head>  
            <title>KL TICKET DATABASE</title>  
             
@@ -22,3 +63,39 @@
    </tr>  
                           </thead>  
                           <tbody>
+
+
+
+<?php
+
+
+$sql_transactions="SELECT * FROM `transactions`  WHERE  `email`='".$_SESSION['email']."' " ;
+$result = $connect->query($sql_transactions);
+while($row = $result->fetch_assoc()){
+
+echo'<tr class="class="table table-striped table-bordered"">
+        <td>'.$row["Date"].'</td>
+        <td>'.$row["T_No."].'</td>
+        <td>'.$row["Name"].'</td>
+        <td>'.$row["source"].'</td>
+        <td>'.$row["dest"].'</td>
+        <td>'.$row["Class"].'</td>
+        <td>'.$row["Type"].'</td>
+        <td>₹&nbsp&nbsp'.$row["Amt"].'</td>
+        <td><a  href="print.php?pid='.$row["T_No."].' " target="_blank">Click Here</a></td>
+       
+
+';
+}
+?>
+</tbody>
+</table>
+</div>
+ <script>  
+ $(document).ready(function() {
+    $('#database_table').DataTable( {
+        "order": [[ 1, "desc" ]]
+    } );
+} ); 
+ </script>  
+
