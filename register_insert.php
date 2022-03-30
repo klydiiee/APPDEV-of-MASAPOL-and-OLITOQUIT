aed43e38ed54ad3e9a098480de177ad3ca452613
klydiiee <57524612+klydiiee@users.noreply.github.com>
2022-03-30 11:41:58 +0800

    Create register_insert.php

---
+++ register_insert.php
@@ -0,0 +1,7 @@
+<! Script to add newly registered people in to the Database -->
+
+<style >  .table{  font-family: Montserrat, sans-serif;
+align-items: center}
+
+</style>
+<link rel="stylesheet" type="text/css" href="index.css">
