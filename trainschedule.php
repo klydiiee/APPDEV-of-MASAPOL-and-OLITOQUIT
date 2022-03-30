3dcdfd0c15b598c1e877e304e5bac689c2e64b8b
klydiiee <57524612+klydiiee@users.noreply.github.com>
2022-03-30 13:26:32 +0800

    Update trainschedule.php

--- trainschedule.php
+++ trainschedule.php
@@ -9,4 +9,19 @@ table#database_table {
     border-collapse: collapse;
     border-spacing: 0;
 }
+
+#database_table td, #database_table th {
+    border: 1px solid #ddd;
+    text-align: left;
+    padding: 8px;
+}
+
+#database_table tr:nth-child(even){background-color: #f2f2f2}
+
+#database_table th {
+    padding-top: 11px;
+    padding-bottom: 11px;
+    background-color: black;
+    color: white;
+}
 </style>
